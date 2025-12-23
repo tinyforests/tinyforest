1288c772794e97855284274dc7702427150eeb71
tinyforests <tyson@gardenerandson.com>
2025-12-24 09:47:59 +1100

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    NOMINATIM_URL: 'https://nominatim.openstreetmap.org',
+    VIC_WFS_URL: 'https://opendata.maps.vic.gov.au/geoserver/wfs',
+    EVC_LAYER: 'open-data-platform:nv2005_evcbcs'
+};
